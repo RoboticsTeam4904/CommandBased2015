ccab25f7f7a25615eb2e4e84ee7cfd2422e2fc4c
c0d3rman <yonilern@gmail.com>
2015-02-15 13:56:17 -0800

    Small fix

--- CommandBased/src/org/usfirst/frc4904/cmdbased/XboxDrive.java
+++ CommandBased/src/org/usfirst/frc4904/cmdbased/XboxDrive.java
@@ -15,12 +15,12 @@ public class XboxDrive extends CommandBase {
 	
 	public void execute() {
 		// Calculations
-		double xMovement = CommandBase.oi.xbox.getValue(RobotMap.XBOX_Y_STICK) * RobotMap.X_SPEED_SCALE;
-		double yMovement = CommandBase.oi.xbox.getValue(RobotMap.XBOX_X_STICK) * RobotMap.Y_SPEED_SCALE;
+		double xMovement = oi.xbox.getValue(RobotMap.XBOX_Y_STICK) * RobotMap.X_SPEED_SCALE;
+		double yMovement = oi.xbox.getValue(RobotMap.XBOX_X_STICK) * RobotMap.Y_SPEED_SCALE;
 		double[] data = MecanumHelper.cartesianToPolar(xMovement, yMovement);
 		double speed = data[0];
 		double angle = data[1];
-		double turnSpeed = CommandBase.oi.xbox.getValue(RobotMap.XBOX_TWIST_STICK) * RobotMap.TURN_SPEED_SCALE;
+		double turnSpeed = oi.xbox.getValue(RobotMap.XBOX_TWIST_STICK) * RobotMap.TURN_SPEED_SCALE;
 		// Commands
 		Scheduler.getInstance().add(new ChassisDrive(speed, angle, turnSpeed));
 	}
