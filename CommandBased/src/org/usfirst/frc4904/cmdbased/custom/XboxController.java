acf81d76ac9b5260d7a5917e38fc4826ada7b408
c0d3rman <yonilern@gmail.com>
2015-02-14 23:16:06 -0800

    Moved Xbox constants to RobotMap

--- CommandBased/src/org/usfirst/frc4904/cmdbased/custom/XboxController.java
+++ CommandBased/src/org/usfirst/frc4904/cmdbased/custom/XboxController.java
@@ -1,47 +1,26 @@
 package org.usfirst.frc4904.cmdbased.custom;
 
 
+import org.usfirst.frc4904.cmdbased.RobotMap;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class XboxController extends Joystick {
-	public final static int A_BUTTON = 1;
-	public final static int B_BUTTON = 2;
-	public final static int X_BUTTON = 3;
-	public final static int Y_BUTTON = 4;
-	public final static int LEFT_BUMPER = 5;
-	public final static int RIGHT_BUMPER = 6;
-	public final static int BACK_BUTTON = 7;
-	public final static int START_BUTTON = 8;
-	public final static int LEFT_STICK = 9;
-	public final static int RIGHT_STICK = 10;
-	public final static int X_STICK = 0; // Left x
-	public final static int Y_STICK = 1; // Left y
-	public final static int TWIST_STICK = 2; // Button pair on front. Left is twist, trying to determine right
-	private SuperButton[] buttons = new SuperButton[10];
-	
 	public XboxController(int port) {
 		super(port);
-		for (int i = 0; i < 10; i++) {
-			buttons[i] = new SuperButton(this, i + 1);
-		}
 	}
-	
+
 	public double getValue(int axis) {
 		double value = 0;
-		if (axis == X_STICK) {
+		if (axis == RobotMap.XBOX_X_STICK) {
 			value = this.getX(); // What this is doing should make sense
-		} else if (axis == Y_STICK) {
+		} else if (axis == RobotMap.XBOX_Y_STICK) {
 			value = this.getY(); // Ditto above
-		} else if (axis == TWIST_STICK) {
-			value = getRawAxis(4); // Stick mappings here: http://www.chiefdelphi.com/forums/showpost.php?p=1003245&postcount=8
+		} else if (axis == RobotMap.XBOX_TWIST_STICK) {
+			value = getRawAxis(RobotMap.XBOX_TWIST_RAW_AXIS);
 		}
-		if (Math.abs(value) < 0.1) { // Xbox does not go perfectly to zero when released
-			value = 0.0; // Do it ourselves
-		} // Software to solve hardware problems. Maybe not great.
+		if (Math.abs(value) < RobotMap.XBOX_MINIMUM_THRESHOLD) { // Xbox does not go perfectly to zero when released
+			value = 0;
+		}
 		return value;
-	}
-	
-	public boolean getButton(int button) {
-		return buttons[button].get();
 	}
 }
