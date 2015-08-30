6b3e6b1283283f0c65bada4b01f8adc70ac55b1d
Carter Turnbaugh <carter@localhost.localdomain>
2015-08-30 12:31:39 -0700

    Updatedb cmdbased. Added LiveWindow to Robot.java

--- src/org/usfirst/frc4904/robot/Robot.java
+++ src/org/usfirst/frc4904/robot/Robot.java
@@ -43,6 +43,7 @@ public class Robot extends CommandRobotBase {
 		// Display choosers on SmartDashboard
 		displayChoosers();
 		SmartDashboard.putData(Scheduler.getInstance());
+		RobotMap.pdp.addLiveWindow();
 	}
 	
 	public void autonomousInit() {
