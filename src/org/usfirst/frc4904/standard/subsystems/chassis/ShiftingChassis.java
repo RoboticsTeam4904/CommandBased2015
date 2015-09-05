71cda6f52490df8245ae5101e2a8202e268c7e3f
Carter Turnbaugh <carterturn@tutanota.de>
2015-09-04 17:16:45 -0700

    Fixed git submodule
    Wasn't actually a submodule.
    As this is a CRITICAL repository problem that will not cause compilation errors
    (although the name change has caused other problems, which I will fix soon)
    I am not branching for this.

--- src/org/usfirst/frc4904/standard/subsystems/chassis/ShiftingChassis.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc4904.cmdbased.subsystems.chassis;
-
-
-public interface ShiftingChassis {
-	/**
-	 * @return an array of Solenoids (should return from left to right)
-	 */
-	public SolenoidShifters getShifter();
-}
